9d2f4ed505fe6ec681050a13d0e05cbed5e0a765
residentevil0803 <ramseyklassen0803@gmail.com>
2022-06-15 05:40:13 +0900

    dbUri

--- src/database/config.service.ts
+++ src/database/config.service.ts
@@ -11,7 +11,7 @@ export class DBUri {
     const host = this.env.get('MONGODB_HOST');
     const dbname = this.env.get('MONGODB_DBNAME');
 
-    this.uri = `mongodb+srv://${username}:${password}@${host}/${dbname}?retryWrites=true&w=majority`;
+    this.uri = `mongodb+srv://${username}:${password}@${host}/${dbname}`;
     // this.uri = "mongodb://localhost:27017/digikraft";
   }
 }
